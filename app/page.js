e0f406c55ffd2bc57c49c91791ec88256781074c
bastientrevisan <bastien.trevisan@gmail.com>
2025-04-21 18:58:31 +0200

    [DEV] Ajout API routes pour Mongo + Panneau admin

--- app/page.js
+++ app/page.js
@@ -2,55 +2,45 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  try {
-    const [latest, setLatest] = useState([]);
+  const [latest, setLatest] = useState([]);
 
-    useEffect(() => {
-      const fetchlatestArticle = async () => {
-        const response = await fetch("/api/articles/latest");
-        const data = await response.json();
-        setLatest(data);
-      };
+  useEffect(() => {
+    const fetchlatestArticle = async () => {
+      const response = await fetch("/api/articles/latest");
+      const data = await response.json();
+      setLatest(data);
+    };
 
-      fetchlatestArticle();
-    }, []);
+    fetchlatestArticle();
+  }, []);
 
-    const article = latest[0] || null;
+  const article = latest[0] || null;
 
-    return (
-      <div className="m-8 flex">
-        <div className="m-8">
-          <h1 className="text-xl font-bold">Dernier article</h1>
-          {article ? (
-            <div key={article._id} className="card bg-neutral shadow-sm">
-              <div className="card-body">
-                <h2 className="card-title">{article.titre}</h2>
-                <p>{article.contenu}</p>
-              </div>
-            </div>
-          ) : (
-            <p>Aucun article trouvé</p>
-          )}
-        </div>
-
-        <div className="m-8 w-250">
-          <h1 className="text-xl font-bold">Actu reseaux</h1>
-          <div className="card bg-neutral shadow-sm">
+  return (
+    <div className="m-8 flex">
+      <div className="m-8">
+        <h1 className="text-xl font-bold">Dernier article</h1>
+        {article ? (
+          <div key={article._id} className="card bg-neutral shadow-sm">
             <div className="card-body">
-              <h2 className="card-title"></h2>
-              <p>Integration post insta / facebook</p>
+              <h2 className="card-title">{article.titre}</h2>
+              <p>{article.contenu}</p>
             </div>
+          </div>
+        ) : (
+          <p>Aucun article trouvé</p>
+        )}
+      </div>
+
+      <div className="m-8 w-250">
+        <h1 className="text-xl font-bold">Actu reseaux</h1>
+        <div className="card bg-neutral shadow-sm">
+          <div className="card-body">
+            <h2 className="card-title"></h2>
+            <p>Integration post insta / facebook</p>
           </div>
         </div>
       </div>
-    );
-  } catch (error) {
-    console.error("Error fetching latest article:", error);
-    return (
-      <div className="m-8">
-        <h1 className="text-xl font-bold">Erreur</h1>
-        <p>Une erreur est survenue lors du chargement des articles.</p>
-      </div>
-    );
-  }
+    </div>
+  );
 }
